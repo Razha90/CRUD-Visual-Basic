a10f32049762fecb26327d6c884d771a1b562e0f
razha90 <razhajamsiksyah@gmail.com>
2023-11-22 14:50:39 +0700

    first commit

---
+++ Kampus/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(1, 246)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(798, 202)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(125, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nim"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(125, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nama"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(125, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Alamat"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(125, 154)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Jurusan"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(223, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(328, 26)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(223, 73)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(328, 26)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(223, 117)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(328, 26)
+        Me.TextBox3.TabIndex = 7
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(223, 154)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(328, 26)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(143, 200)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(97, 40)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Simpan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(279, 200)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(97, 40)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Edit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(419, 200)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(97, 40)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Hapus"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(558, 200)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(97, 40)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "Delete"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Kampus/Form1.vb
@@ -0,0 +1,195 @@
+﻿Imports System.Data.Odbc
+Imports System.Reflection
+
+Public Class Form1
+
+    Dim Conn As OdbcConnection
+    Dim Cmd As OdbcCommand
+    Dim Ds As DataSet
+    Dim Da As OdbcDataAdapter
+    Dim Rd As OdbcDataReader
+    Dim MyDB As String
+    'Membuat Koneksi
+    Sub Koneksi()
+        ' Memanggil database yaitu nama database kita adalah db_universitas
+        MyDB = "DSN=Mantap;"
+        Conn = New OdbcConnection(MyDB)
+        If Conn.State = ConnectionState.Closed Then Conn.Open()
+    End Sub
+
+    ' Memanggil Kondisi awal
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call KondisiAwal()
+    End Sub
+
+    ' Pertama kali dipanggil dengan membuat Sub KondisiAwal()
+    Sub KondisiAwal()
+        ' Textbox1, textbox2, textbox3, textbox4 kita kosongkan pertama kali
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        ' Textbox1, textbox2, textbox3, textbox4.enabled = false artinya formnya kita matikan
+        TextBox1.Enabled = False
+        TextBox2.Enabled = False
+        TextBox3.Enabled = False
+        TextBox4.Enabled = False
+        ' Textbox1 yang berarti form nim kita maksimalkan hanya bisa 15 huruf/angka
+        TextBox1.MaxLength = 15
+        ' Button1, Button2, Button3, Button4 kita tambahkan text masing -masing yaitu input, edit, hapus, tutup
+        Button1.Text = "INPUT"
+        Button2.Text = "EDIT"
+        Button3.Text = "HAPUS"
+        Button4.Text = "TUTUP"
+        ' Button1, Button2, Button3, Button4 kita aktifkan dengan menggunakan perintah true
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        ' Panggil koneksi yang sudah kita buat sub Koneksi()
+        Call Koneksi()
+        ' Memanggil table yang sudah kita buat yaitu mahasiswa
+        Da = New OdbcDataAdapter("Select * From mahasiswa", Conn)
+        Ds = New DataSet
+        Da.Fill(Ds, "mahasiswa")
+        DataGridView1.DataSource = Ds.Tables("mahasiswa")
+    End Sub
+
+    Sub FieldAktif()
+        ' untuk mengaktifkan form
+        TextBox1.Enabled = True
+        TextBox2.Enabled = True
+        TextBox3.Enabled = True
+        TextBox4.Enabled = True
+        TextBox1.Focus()
+    End Sub
+    ' Ini adalah proses simpan
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' if button1.text yang textnya "input" maka akan berubah menjadi button "simpan"
+        If Button1.Text = "INPUT" Then
+            Button1.Text = "SIMPAN"
+            ' lalu button2 dan button3 tidak aktif dan button4 kita ubah menjadi tulisan "batal"
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Text = "BATAL"
+            ' lalu kita panggil FieldAktif() yang mana textbox1,textbox2,textbox3,textbox4 akan diaktifkan
+            Call FieldAktif()
+        Else
+            ' if textbox1, textbox2, textbox3 dan textbox3 kosong maka muncul alert pastikan semua field terisi
+            ' ini artinya disebut validasi
+            If TextBox1.Text = "" Or TextBox2.Text = "" Or
+                TextBox3.Text = "" Or TextBox4.Text = "" Then
+                MsgBox("Pastikan semua field terisi")
+            Else
+                ' Jika semua form terisi, maka kita panggil Koneksi() ke database
+                Call Koneksi()
+                ' lalu kita masukan data kita ke table mahasiswa (insert into mahasiswa .....)
+                Dim InputData As String = "Insert into mahasiswa values('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "','" & TextBox4.Text & "')"
+                Cmd = New OdbcCommand(InputData, Conn)
+                Cmd.ExecuteNonQuery()
+                ' lalu kita tampilkan message "input data berhasil"
+                MsgBox("Input data berhasil")
+                ' lalu kita kembalikan ke KondisiAwal()
+                Call KondisiAwal()
+            End If
+        End If
+    End Sub
+    ' Ini adalah proses EDIT
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'If Button2 edit kita klik maka dia akan berubah menjadi tulisan "update"
+        If Button2.Text = "EDIT" Then
+            Button2.Text = "UPDATE"
+            'button1 dan button2 akan kita false artinya tidak berfungsi
+            Button1.Enabled = False
+            Button3.Enabled = False
+            'button4 kita ganti tulisan menjadi batal
+            Button4.Text = "BATAL"
+            ' Lalu kita panggil FieldAktif() yang mana textbox1,TextBox2, TextBox3 dan textbox 4 kita aktifkan
+            Call FieldAktif()
+        Else
+            ' ini adalah validasi jika textbox1, textbox2, TextBox3 dan textbox 4 tidak terisi maka akan muncul alert("pastikan semua terisi)
+            If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Then
+                MsgBox("Pastikan semua field terisi")
+            Else
+                ' jika semua terisi panggil Koneksi()
+                Call Koneksi()
+                ' kita update table mahasiswa
+                Dim EditData As String = "Update mahasiswa set nama ='" & TextBox2.Text & "',alamat='" & TextBox3.Text & "',jurusan='" & TextBox4.Text & "'where nim='" & TextBox1.Text & "'"
+                Cmd = New OdbcCommand(EditData, Conn)
+                Cmd.ExecuteNonQuery()
+                ' jika berhasil tampilkan alert / message ("edit Data berhasil")
+                MsgBox("Edit data berhasil")
+                ' setelah semua sudah tolong tampilkan
+                KondisiAwal()
+                Call KondisiAwal()
+            End If
+        End If
+    End Sub
+
+    ' Button3 adalah button hapus
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Jika button3.text yang mana textnya adalah hapus maka kita ubah texttnya menjadi delete
+        If Button3.Text = "HAPUS" Then
+            Button3.Text = "DELETE"
+            ' button1 non aktifkan
+            Button1.Enabled = False
+            ' button2 juga kita non aktifkan, jadi dia ga bisa di klik sama sekali ketika button hapus kita klik
+            Button2.Enabled = False
+            ' button4.text kita ubah textnya dari tutup menjadi batal
+            Button4.Text = "BATAL"
+            ' setelah itu kita aktifkan FieldAktif() yang mana artinya kita mengaktifkan textbox1, textbox2, textbox3 dan TextBox4
+            Call FieldAktif()
+        Else
+            ' Ini adalah validasi
+            ' jika field tidak terisi maka tidak akan bisa di hapus
+            If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Then
+                MsgBox("Pastikan data yang akan dihapus terisi")
+            Else
+               ' jika sudah kita isi fieldnya maka bisa kita hapus, prosesnya adalah
+               ' kita panggil Koneksi()
+               Call Koneksi()
+                ' lalu kita panggil table mahasiswa lalu dia bilang "tolongin aku dong, aku mau hapus dengan nim xxx tolong di bantu ya.makasih: )"
+                Dim HapusData As String = "delete from mahasiswa where nim ='" & TextBox1.Text & "'"
+                Cmd = New OdbcCommand(HapusData, Conn)
+                Cmd.ExecuteNonQuery()
+                ' kalau berhasil kita tampilkan alert / message dengan tulisan "hapus data berhasil"
+                MsgBox("Hapus data berhasil")
+            ' lalu kita panggil kondisiAwal()
+            Call KondisiAwal()
+        End If
+        End If
+    End Sub
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        ' if chr 13 kita tekan, btw chr 13 itu adalah enter ya
+        If e.KeyChar = Chr(13) Then
+            ' setelah tekan entar maka Koneksi() akan terpanggil
+            Call Koneksi()
+            ' lalu ketika kita masukan nim di textbox1 (tekan enter)
+            ' selanjutnya tolong panggilkan table mahasiswa dan tolong isi isi form textbox2 dan textbox3 dan textbox4
+            Cmd = New OdbcCommand("Select * from mahasiswa where nim='" & TextBox1.Text & "'", Conn)
+            Rd = Cmd.ExecuteReader
+            Rd.Read()
+            ' if kita panggil nim lalu tekan enter maka otomatis TextBox2, textbox3, dan textbox4 akan terisi secara otomatis
+            If Rd.HasRows Then
+                TextBox2.Text = Rd.Item("nama")
+                TextBox3.Text = Rd.Item("alamat")
+                TextBox4.Text = Rd.Item("jurusan")
+            Else
+                ' Jika nim yang kita ketikan salah, maka akan menampilkan alert atau message ("data tidak ada")
+                MsgBox("Data Tidak Ada")
+            End If
+        End If
+    End Sub
+
+    ' ini adalah proses button4 yaitu tutup
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ' Jika button4 textnya TUTUP maka tolong end atau exit atau keluar dari aplikasi
+        If Button4.Text = "TUTUP" Then
+            End
+            ' namun kalau tulisannya BATAL maka kita panggil
+            KondisiAwal()
+        Else
+            Call KondisiAwal()
+        End If
+    End Sub
+End Class
